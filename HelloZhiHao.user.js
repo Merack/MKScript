8b55ec919f2e5dd5eb6156c5179aab7b93b357c0
Merack <merack@qq.com>
2021-05-08 17:40:00 +0800

    [+]add script: HelloZhiHao.user.js

---
+++ HelloZhiHao.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         zhihaoAutoDo
+// @namespace    https://merack.top/
+// @version      0.1
+// @description  complete homework automatically in www.zhihao.com!
+// @author       Merack
+// @e-mail       Merack@qq.com
+// @match        http://www.zhihao.com/jrwtest*/*.asp
+// @match        http://www.zhihao.com/jrwtest*/*.htm
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.slim.min.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+"use strict";
+
+const FLAG = "optionFlag";
+
+var doc = $("body").text();
+if (doc.indexOf("开始答题") > 0 || doc.indexOf("开始答题") == 0) {
+  $("a")[0].click();
+}
+
+var flag = GM_getValue(FLAG, 0);
+
+var ans = $('input[name="answer1"]');
+var submit = $('input[type="submit"]');
+if (ans.length) {
+  ans.eq(flag).click();
+  submit.click();
+}
+if (doc.indexOf("答案错误") > 0 || doc.indexOf("答案错误") == 0) {
+  flag++;
+  GM_setValue(FLAG, flag);
+  // console.log(flag);
+  $("a")[0].click();
+}
+
+if (doc.indexOf("答案正确") > 0 || doc.indexOf("答案正确") == 0) {
+  GM_setValue(FLAG, 0);
+  $("a")[0].click();
+}
+
+// console.log(doc);
+// console.log(flag);
+// console.log(ans);
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MKScript
 some scripts written by Merack
+
+### 0.如何安装油猴脚本管理器
+各大主流浏览器商店内都能搜到油猴脚本管理器, 推荐violentmonkey和tampermonkey  
+以edge浏览器为例, 可以到以下链接中安装其中一个即可
+[violentmonkey](https://microsoftedge.microsoft.com/addons/detail/violentmonkey/eeagobfjdenkkddmbclomhiblgggliao?hl=en-US)
+[tampermonkey](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd?hl=en-US)
+
+### 1.HelloZhiHao
+智豪校园网自动答题脚本. 由于智豪哥很好人提供无限试错的机会, 所以写了这个脚本暴力填写.
+##### 使用说明
+需先安装油猴脚本管理器, 参照第0点
+然后  [点我](https://cdn.jsdelivr.net/gh/Merack/MKScript/HelloZhiHao.user.js)  进行安装
